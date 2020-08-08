23240123f50ab0ed12d5ba416cac51e50ab12f98
pallavi-gif <pallavibit97@gmail.com>
2020-08-08 20:32:02 +0530

    jenkins job

---
+++ jenkinsjobheloworld/src/jenkinsjobheloworld/Hlow.java
@@ -0,0 +1,12 @@
+package jenkinsjobheloworld;
+
+public class Hlow {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+		System.out.println("HelooooW!");	
+		}
+
+	}
+
+}
